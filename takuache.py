536a1d33b5e54a0638a72d003f5d8db7da47df69
limp-pp <71045004+limp-pp@users.noreply.github.com>
2020-09-09 16:14:26 -0600

    Add files via upload

---
+++ takuache.py
@@ -0,0 +1,11 @@
+takuache = False
+anything = input("si quema cuh?")
+if anything == "yes":
+    takuache = True
+if takuache == True:
+    print("lets race foo")
+if anything == "no":
+    takuache = False
+if takuache == False:
+    print("no es v8")
+    
