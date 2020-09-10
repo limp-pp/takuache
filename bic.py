37db5d9e7a86e2f5ebc45cb15262d2cb549911a4
limp-pp <71045004+limp-pp@users.noreply.github.com>
2020-09-09 20:59:20 -0600

    Add files via upload

---
+++ bic.py
@@ -0,0 +1,26 @@
+cont = True
+while cont == True:
+    print("new phone who dis, it better not be bic")
+    user_name = input()
+
+
+    if user_name == "bic":
+        print ("fak u")
+    elif user_name == "mecos":
+        value = 3
+        while value >2:
+            print("v6")
+    else:
+        print("fak u too")
+    conti = input("do you wanna continue")
+
+    yes = ['yes',' yeah','yey']
+    no = ['no','nah','nope']
+
+    if conti in yes:
+        cont = True
+    elif conti in no:
+        cont = False
+    else:
+        print("you wack")
+print("fak u still")
